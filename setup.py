dfcd84d91f75312e5ed174068847be3f51d77eae
hawry <tomas@hawry.net>
2021-02-02 21:59:19 +0100

    add readme to setup

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 from setuptools import setup, find_packages
 
+from os import path
+
+with open(path.join(path.abspath(path.dirname(__file__)), 'README.md')) as f:
+    long_description = f.read()
+
 setup(
     name='sfunc',
-    version='0.3.0',
+    version='0.3.1',
     packages=find_packages(),
     author='hawry',
     description='Wrappers for using a single lambda function for AWS ApiGateway',
-    url='https://github.com/hawry/single-lambda-api-function',
+    url='https://github.com/hawry/sfunc',
     python_requires='>=3.6',
     tests_requires=[
         "pytest",
         "pytest-cov"
-    ]
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
